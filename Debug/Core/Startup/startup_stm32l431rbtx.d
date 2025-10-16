da04fe0374b910c4cd585d2038143bcabacea5ba
“Christy <[christy.guirguis@ucalgary.ca]>
2025-10-15 19:43:54 -0600

    Remove build artifacts and ignore them

--- Debug/Core/Startup/startup_stm32l431rbtx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32l431rbtx.o: \
- ../Core/Startup/startup_stm32l431rbtx.s
--- Debug/Drivers/LIS3DH_Driver/lis3dh_reg.d
+++
@@ -1,3 +0,0 @@
-Drivers/LIS3DH_Driver/lis3dh_reg.o: ../Drivers/LIS3DH_Driver/lis3dh_reg.c \
- ../Drivers/LIS3DH_Driver/../LIS3DH_Driver/lis3dh_reg.h
-../Drivers/LIS3DH_Driver/../LIS3DH_Driver/lis3dh_reg.h:
